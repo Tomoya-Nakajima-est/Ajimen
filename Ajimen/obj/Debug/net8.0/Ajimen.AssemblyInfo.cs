1600b97c0528e82afa02cb632cdb7899a9ba1e8b
EAST/柳井晴 <yanai@est.co.jp>
2025-06-23 11:19:35 +0900

    ちくわ

--- Ajimen/obj/Debug/net8.0/Ajimen.AssemblyInfo.cs
+++ Ajimen/obj/Debug/net8.0/Ajimen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ajimen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad7ffec20d319626b1a76b22b761aaa14d2410ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ajimen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ajimen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
